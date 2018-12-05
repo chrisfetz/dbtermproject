ff015572e3af51f8a55554d345811031d2538c22
Kevin Bagwell <kev.bag96@gmail.com>
2018-12-05 18:36:02 +0000

    table adjustments for normalization and indices for taxdatasum

---
+++ db/tableadjustments.sql
@@ -0,0 +1,16 @@
+ALTER TABLE restaurants
+    drop latitude, drop longitude, drop county, drop state;
+ 
+DROP TABLE IF  EXISTS `taxdatasum`;
+CREATE TABLE IF NOT EXISTS `termproject`.`taxdatasum` (
+	`id` INT NOT NULL auto_increment,
+    PRIMARY KEY (`id`))
+    SELECT state, zipcode, county, sum(numReturns) as numReturns, sum(totalIncome) as totalIncome
+            from taxdata group by state, zipcode;
+	
+CREATE INDEX idxzip
+ON taxdatasum (zipcode);
+CREATE INDEX idxstate
+ON taxdatasum (state);
+CREATE INDEX idxcounty
+ON taxdatasum (county);
